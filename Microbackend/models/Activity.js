82d69189af24b2cb4b294b4d1fb78d449ae3ffb7
Swapnil Chavan <chavanswapnil822@gmail.com>
2026-07-10 19:32:35 +0530

    updatewd code

--- Microbackend/models/Activity.js
+++ Microbackend/models/Activity.js
@@ -1,106 +1,81 @@
 const ActivitySchema = new mongoose.Schema(
-  {
-    farmerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Farmer",
-      required: true,
+{
+    farmerId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Farmer",
+        required:true,
     },
 
-    activityType: {
-      type: String,
-      enum: ["fertilizer", "water", "harvest", "soil"],
-      required: true,
+    activityType:{
+        type:String,
+        enum:[
+            "fertilizer",
+            "water",
+            "harvest",
+            "soil",
+        ],
+        required:true,
     },
 
-    volume: {
-      type: Number,
-      default: 0,
+    volume:{
+        type:Number,
+        default:0,
     },
 
-    remarks: {
-      type: String,
-      default: "",
+    remarks:{
+        type:String,
+        default:"",
     },
 
-    image: {
-      type: String,
-      default: "",
+    image:{
+        type:String,
+        default:"",
     },
 
-    location: {
-      latitude: String,
-      longitude: String,
+    location:{
+        latitude:String,
+        longitude:String,
     },
 
-    activityDate: {
-      type: Date,
-      default: Date.now,
+    activityDate:{
+        type:Date,
+        default:Date.now,
     },
 
-    // Water
-    motorHP: {
-      type: String,
-      default: "",
+    motorHP:{
+        type:String,
+        default:"",
     },
 
-    startTime: {
-      type: Date,
-      default: null,
+    startTime:{
+        type:String,
+        default:"",
     },
 
-    endTime: {
-      type: Date,
-      default: null,
+    endTime:{
+        type:String,
+        default:"",
     },
 
-    durationHours: {
-      type: Number,
-      default: 0,
+    durationHours:{
+        type:Number,
+        default:0,
     },
 
-    // Soil
-    soilReport: {
-      type: String,
-      default: "",
+    // NEW
+
+    soilReport:{
+        type:String,
+        default:"",
     },
 
-    // Harvest
-    paniclesPerSqm: {
-      type: Number,
-      default: 0,
-    },
-
-    plantHeight: {
-      type: Number,
-      default: 0,
-    },
-
-    leafLength: {
-      type: Number,
-      default: 0,
-    },
-
-    leafWidth: {
-      type: Number,
-      default: 0,
-    },
-
-    panicleLength: {
-      type: Number,
-      default: 0,
-    },
-
-    grainsPerPanicle: {
-      type: Number,
-      default: 0,
-    },
-
-    thousandSeedWeight: {
-      type: Number,
-      default: 0,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    soilType:{
+        type:String,
+        default:"",
+    }, 
+    
+},
+{
+    timestamps:true,
+}
 );
